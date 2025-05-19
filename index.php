88b1dd2ac06bc145ba2961680335a2903a8b17ca
Vinicius OLiveira <vinicius.r.oliveira@outlook.com.br>
2025-05-19 01:16:09 -0300

    iniciando o projeto

--- index.php
+++ index.php
@@ -1,12 +1,23 @@
+<?php 
+include_once("helpers/url.php");
+
+?>
+
 <!DOCTYPE html>
-<html lang="pt">
+<html lang="pt-br">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Blog Codar</title>
-<link rel="stylesheet" href="css/style.css">
+    <!-- CSS -->
+  <link rel="stylesheet" href="<?= $BASE_URL ?>css/styles.css">
+    <!-- GOOGLE FONTS-->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Monsieur+La+Doulaise&family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 </head>
 <body>
+    <h1>Olá Mundo</h1>
     
 </body>
 </html>
